d4d74412ab44a64b9b54ebbd85a805f3d7648953
Kiran <phegaro@gmail.com>
2023-01-22 17:41:57 -0800

    New Admin with Inventory STore Items

--- crossroadsBe/admin.py
+++ crossroadsBe/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Profile, StoreItem, Inventory, Quiz, Play, Feedback, Fact
+from .models import Profile, StoreItem, Inventory, InventoryStoreItem, Quiz, Play, Feedback, Fact
 
 admin.site.register(Profile)
 admin.site.register(StoreItem)
 admin.site.register(Inventory)
+admin.site.register(InventoryStoreItem)
 admin.site.register(Quiz)
 admin.site.register(Play)
 admin.site.register(Feedback)
