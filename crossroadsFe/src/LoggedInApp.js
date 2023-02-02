ee52c5fbe09414959de178c4b158496b977d3196
Kiran <phegaro@gmail.com>
2023-02-01 21:10:57 -0800

    introduce useSWR for

--- crossroadsFe/src/LoggedInApp.js
+++ crossroadsFe/src/LoggedInApp.js
@@ -12,194 +12,193 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, Alert, CircularProgress } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import * as React from "react";
-
+import { useState } from 'react';
+import useSWR from 'swr';
 import IconButton from '@mui/material/IconButton';
-import {
-  CalendarToday, 
-  Inventory, 
-  ShoppingCart, 
-  } from '@mui/icons-material';
+import { CalendarToday, Inventory, ShoppingCart } from '@mui/icons-material';
 
 
 
-class LoggedInApp extends React.Component {
+function LoggedInApp(props) {
+  const { data: profile, error, isLoading } = useSWR('/profile');
+
   
-  constructor(props) {
-    super(props);
-    this.state = {
-      showHowToPlay: true,
-      dropdownLead: false,
-      htpOpen: false,
-      dfOpen: false,
-      leads: false,
-      leadMenu: false,
-      calendarOpen: false,
-      inventoryOpen: false,
-      voteOpen: false,
-      profileOpen: false,
-      storeOpen: false,
-      leftright: false,
-      feedbackOpen: false,
-      aboutUsOpen: false,
-    };
-  }
+  const [state, setState] = useState({
+    showHowToPlay: true,
+    dropdownLead: false,
+    htpOpen: false,
+    dfOpen: false,
+    leads: false,
+    leadMenu: false,
+    calendarOpen: false,
+    inventoryOpen: false,
+    voteOpen: false,
+    profileOpen: false,
+    storeOpen: false,
+    leftright: false,
+    feedbackOpen: false,
+    aboutUsOpen: false,
+  });
 
-  handleOpenClose(stateVar) {
-    var newState = {};
-    newState[stateVar] = !this.state[stateVar];
+
+  function handleOpenClose(stateVar) {
+    var newState = Object.assign({}, state);
+    newState[stateVar] = !state[stateVar];
     if (stateVar === 'voteOpen') {
-      if (this.state.leftright === true) {
-        this.setState(newState)
+      if (state.leftright === true) {
+        setState(newState)
       }
     }
     else {
-      this.setState(newState);
+      setState(newState);
     }
   }
-  
-  render () {
-    return (
-      <div className="App">
 
-        <Grid container spacing={2}>
-            
-          <Grid item xs={4}>
-            <Button variant = "outlined" onClick = {this.handleOpenClose.bind(this, 'htpOpen')}>
-              How to Play
-            </Button>
-            <HTPDialog
-              open={this.state.htpOpen}
-              onClose={this.handleOpenClose.bind(this, 'htpOpen')}
-            />
-          </Grid>
-          <Grid item xs={4}>
-            <WhichLeaderboard open={this.state.dfOpen} onClick={this.handleOpenClose.bind(this, 'leads')} leadClickOpen={this.handleOpenClose.bind(this, 'leadMenu')} />
-          </Grid>
+  if (isLoading) {
+    return (<CircularProgress color="secondary" />);
+  }
 
-          <Grid item xs={4}>
-            <Button variant = "outlined" onClick = {this.handleOpenClose.bind(this, 'dfOpen')}>
-              Daily Fact 
-            </Button>
-            <DFDialog
-              open={this.state.dfOpen}
-              onClose={this.handleOpenClose.bind(this, 'dfOpen')}
-            />
-          </Grid>
+  if (error) {
+    return (<Alert severity="error">There is an error {error}</Alert>)
+  }
 
-          <Grid item xs={4}>
-            Current Streak: 
-          </Grid>
+  return (
+    <div className="App">
 
-          <Grid item xs={4}>
-            <IconButton aria-label="inventory" size = "large" onClick = {this.handleOpenClose.bind(this, 'inventoryOpen')}>
-              <Inventory sx = {{ fontSize : 45, border: 2}}/>
-            </IconButton>
-            <InventoryCarousel
-              open={this.state.inventoryOpen}
-              onClose={this.handleOpenClose.bind(this, 'inventoryOpen')}
-            />
-          </Grid>
+      <Grid container spacing={2}>
 
-          <Grid item xs={4}>
-            Best Streak: 
-          </Grid>
+        <Grid item xs={4}>
+          <Button variant="outlined" onClick={() => {handleOpenClose('htpOpen')}}>
+            How to Play
+          </Button>
+          <HTPDialog
+            open={state.htpOpen}
+            onClose={() => {handleOpenClose('htpOpen')}}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <WhichLeaderboard open={state.dfOpen} onClick={() => {handleOpenClose('leads')}} leadClickOpen={() => {handleOpenClose('leadMenu')}} />
+        </Grid>
 
-          
-          <Grid item xs={3}>
-            <Button disabled={this.state.leftright} onClick={this.handleOpenClose.bind(this, 'leftright')}>
-              Left
-            </Button>
-          </Grid>
+        <Grid item xs={4}>
+          <Button variant="outlined" onClick={() => {handleOpenClose('dfOpen')}}>
+            Daily Fact
+          </Button>
+          <DFDialog
+            open={state.dfOpen}
+            onClose={() => {handleOpenClose('dfOpen')}}
+          />
+        </Grid>
 
-          <Grid item xs={6}>
-              <img src={logo} className="App-logo" alt="logo" sx = {{width: "100%", height: "100%"}}/>
-          </Grid>
+        <Grid item xs={4}>
+          Current Streak: {profile.streak}
+        </Grid>
 
-          <Grid item xs={3}>
-            <Button disabled={this.state.leftright && this.state.voteOpen} onClick={this.handleOpenClose.bind(this, 'leftright')}>
-              Right
-            </Button>
-          </Grid>
+        <Grid item xs={4}>
+          <IconButton aria-label="inventory" size="large" onClick={() => {handleOpenClose('inventoryOpen')}}>
+            <Inventory sx={{ fontSize: 45, border: 2 }} />
+          </IconButton>
+          <InventoryCarousel
+            open={state.inventoryOpen}
+            onClose={() => {handleOpenClose('inventoryOpen')}}
+          />
+        </Grid>
 
-
-          <Grid item xs={12}>
-            <Button variant="contained" disabled={this.state.voteOpen && this.state.leftright} onClick = {this.handleOpenClose.bind(this, 'voteOpen')}>
-              Vote
-            </Button>
-          </Grid>
-
-          <Grid item xs={4}>
-          
-            <IconButton aria-label="shopping-cart" size = "large" onClick = {this.handleOpenClose.bind(this, 'storeOpen')}>
-              <ShoppingCart sx = {{ fontSize : 60, border: 2}}/>
-              
-            </IconButton>
-            <StoreCarousel
-              open={this.state.storeOpen}
-              onClose={this.handleOpenClose.bind(this, 'storeOpen')}
-            />
-
-          </Grid>
-          
-          <Grid item xs={4}>
-            <Typography variant = "h4" sx = {{padding: 1}}>
-              My Points: 
-            </Typography>
-          </Grid>
-          
-
-          <Grid item xs={4}>
-          
-            <IconButton aria-label="calendar-today" size = "large" onClick = {this.handleOpenClose.bind(this, 'calendarOpen')}>
-              <CalendarToday sx = {{ fontSize : 60, border: 2}}/>
-            </IconButton>
-
-            <CalendarDialog
-              open={this.state.calendarOpen}
-              onClose={this.handleOpenClose.bind(this, 'calendarOpen')}
-            />
-
-          </Grid>
-
-          <Grid item xs={12} fontSize = "large">
-            <Typography variant = "h2" sx = {{padding: 1}}>
-              Welcome to Crossroads!
-            </Typography>
-          </Grid>
-              
-          <Grid item xs = {12}>
-            <ResponsiveAppBar
-            profileOpen = {this.state.profileOpen}
-            profileClose = {this.handleOpenClose.bind(this, 'profileOpen')}
-            profileClickOpen = {this.handleOpenClose.bind(this, 'profileOpen')}
-            feedbackOpen = {this.state.feedbackOpen}
-            feedbackClose = {this.handleOpenClose.bind(this, 'feedbackOpen')}
-            feedbackClickOpen = {this.handleOpenClose.bind(this, 'feedbackOpen')}
-            aboutUsOpen = {this.state.aboutUsOpen}
-            aboutUsClose = {this.handleOpenClose.bind(this, 'aboutUsOpen')}
-            aboutUsClickOpen = {this.handleOpenClose.bind(this, 'aboutUsOpen')}
-            handleLogOut = {this.props.handleLogOut}
-            />
-          </Grid>
-
-
+        <Grid item xs={4}>
+          Best Streak: {profile.highestStreak}
         </Grid>
 
 
+        <Grid item xs={3}>
+          <Button disabled={state.leftright} onClick={() => {handleOpenClose('leftright')}}>
+            Left
+          </Button>
+        </Grid>
+
+        <Grid item xs={6}>
+          <img src={logo} className="App-logo" alt="logo" sx={{ width: "100%", height: "100%" }} />
+        </Grid>
+
+        <Grid item xs={3}>
+          <Button disabled={state.leftright && state.voteOpen} onClick={() => {handleOpenClose('leftright')}}>
+            Right
+          </Button>
+        </Grid>
 
 
-        
-      </div>
+        <Grid item xs={12}>
+          <Button variant="contained" disabled={state.voteOpen && state.leftright} onClick={() => {handleOpenClose('voteOpen')}}>
+            Vote
+          </Button>
+        </Grid>
+
+        <Grid item xs={4}>
+
+          <IconButton aria-label="shopping-cart" sie="large" onClick={() => {handleOpenClose('storeOpen')}}>
+            <ShoppingCart sx={{ fontSize: 60, border: 2 }} />
+
+          </IconButton>
+          <StoreCarousel
+            open={state.storeOpen}
+            onClose={() => {handleOpenClose('storeOpen')}}
+          />
+
+        </Grid>
+
+        <Grid item xs={4}>
+          <Typography variant="h4" sx={{ padding: 1 }}>
+            My Points: {profile.points}
+          </Typography>
+        </Grid>
+
+
+        <Grid item xs={4}>
+
+          <IconButton aria-label="calendr-today" sze="large" onClick={() => {handleOpenClose('calendarOpen')}}>
+            <CalendarToday sx={{ fontSize: 60, border: 2 }} />
+          </IconButton>
+
+          <CalendarDialog
+            open={state.calendarOpen}
+            onClose={() => {handleOpenClose('calendarOpen')}}
+            />
+        </Grid>
+
+      <Grid item xs={12} fontSize="large">
+          <Typography variant="h2" sx={{ padding: 1 }}>
+            Welcome to  C rossroads!
+          </Typography>
+      </Grid>
+
+      <Grid item xs={12}>
+        <ResponsiveAppBar
+          profileOpen={state.profileOpen}
+          profileClose={() => {handleOpenClose('profileOpen')}}
+          profileClickOpen={() => {handleOpenClose('profileOpen')}}
+          feedbackOpen={state.feedbackOpen}
+          feedbackClose={() => {handleOpenClose('feedbackOpen')}}
+          feedbackClickOpen={() => {handleOpenClose('feedbackOpen')}}
+          aboutUsOpen={state.aboutUsOpen}
+          aboutUsClose={() => {handleOpenClose('aboutUsOpen')}}
+          aboutUsClickOpen={() => {handleOpenClose('aboutUsOpen')}}
+          handleLogOut={props.handleLogOut}
+        />
+      </Grid>
+
+
+    </Grid>
+
+
+
+
+
+      </div >
     );
 
-            }
-  
-  
 }
-
 
 
 export default LoggedInApp;
