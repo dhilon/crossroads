796fe2edbce4d1b0d31bfb86d7c6a059f69769e3
dhilon <70111176+dhilon@users.noreply.github.com>
2023-08-06 15:33:17 -0700

    no environ

--- crossroads/settings.py
+++ crossroads/settings.py
@@ -12,17 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import environ
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
