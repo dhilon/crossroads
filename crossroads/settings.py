8545aa5ba46d4c30f654d7a9b82f04a6bd46b9cd
Kiran <phegaro@gmail.com>
2023-08-06 16:20:32 -0700

    Changes to get just a single quiz instead of all quizzes

--- crossroads/settings.py
+++ crossroads/settings.py
@@ -12,16 +12,24 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5__ce-bsii4_unnut!7yu1=-eg6z$a0hg&u#f#+9uwz_^swk=%'
+# SECRET_KEY = 'django-insecure-5__ce-bsii4_unnut!7yu2=-eg6z$a0hg&u#f#+9uwz_^swk=%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
